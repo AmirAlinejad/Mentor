9696f27b5fa0e0227fb11e8ce8e3553adda759a7
AmirAlinejad <aalinejad15@gmail.com>
2024-02-10 00:32:22 -0500

    progress

---
+++ screens/Chat.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const Chat = ({navigation}) => {
+  return (
+    <View>
+      <Text>Chats</Text>
+    </View>
+  );
+};
+
+export default Chat;
